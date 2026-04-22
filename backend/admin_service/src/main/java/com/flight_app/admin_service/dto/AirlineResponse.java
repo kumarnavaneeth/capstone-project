aa6028dd60e31bbc0bf0a18ccd77b847a2dfad29
megha123 <traineemegha@gmail.com>
2026-04-22 14:15:26 +0530

    updated admin_service

--- backend/admin_service/src/main/java/com/flight_app/admin_service/dto/AirlineResponse.java
+++ backend/admin_service/src/main/java/com/flight_app/admin_service/dto/AirlineResponse.java
@@ -3,22 +3,30 @@ package com.flight_app.admin_service.dto;
 public class AirlineResponse {
 	private Long id;
 	private String name;
-    private String status;
+	private Boolean isBlocked;
     
+ public AirlineResponse() {}
+    public AirlineResponse(Long id, String name, Boolean isBlocked) {
+		this.id = id;
+        this.name = name;
+        this.isBlocked = isBlocked;
+    }
     public Long getId() {
 		return id;
 	}
+    public void setId() {
+    	this.id=id;
+    }
 	public String getName() {
 		return name;
 	}
 	public void setName(String name) {
 		this.name = name;
 	}
-	public String getStatus() {
-		return status;
+	public Boolean getIsBlocked() {
+		return isBlocked;
 	}
-	public void setStatus(String status) {
-		this.status = status;
-	}
-	
+	 public void setIsBlocked(Boolean isBlocked) {
+		this.isBlocked = isBlocked;
+		}
 }
