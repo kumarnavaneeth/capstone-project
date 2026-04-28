c907967745ef4146bc16460915556857f0625de4
Navaneeth Kumar <knavaneeth865@gmail.com>
2026-04-28 16:25:29 +0530

    cancel ticket with pnr,also implemented cancel not allowed within 24hr of departure using mockdb json server

--- backend/ticket_service/src/main/java/com/flightapp/ticket_service/controller/TicketController.java
+++ backend/ticket_service/src/main/java/com/flightapp/ticket_service/controller/TicketController.java
@@ -3,6 +3,7 @@ package com.flightapp.ticket_service.controller;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PatchMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
@@ -30,5 +31,10 @@ public class TicketController {
 	public ResponseEntity<Long> bookTicket(@PathVariable Long flightId,@Valid @RequestBody Booking booking) {
 		Booking savedBooking=ticketService.bookTicket(flightId, booking);
 		return ResponseEntity.status(201).body(savedBooking.getBookingId());
+	}
+	@PatchMapping("/booking/cancel/{pnr}")
+	public ResponseEntity<String> cancelTicketByPnr(@PathVariable String pnr){
+		ticketService.cancelTicketByPnr(pnr);
+		return ResponseEntity.ok("Ticket cancelled Successfully");
 	}
 }
