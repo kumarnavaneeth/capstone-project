699c161d0ba58f6a7b48059bf2f541325f32633f
thom98592 <thom98592@gmail.com>
2026-04-29 10:06:36 +0530

    Basic test cases for flight service and controller

--- backend/flight_service/src/main/java/com/flightapp/flight_service/dto/SearchRequest.java
+++ backend/flight_service/src/main/java/com/flightapp/flight_service/dto/SearchRequest.java
@@ -1,8 +1,10 @@
 package com.flightapp.flight_service.dto;
 
 import com.flightapp.flight_service.exceptions.ValidationConstants;
-import lombok.Data;
 
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
 import jakarta.validation.constraints.FutureOrPresent;
 import jakarta.validation.constraints.Min;
 import jakarta.validation.constraints.NotBlank;
@@ -12,6 +14,8 @@ import jakarta.validation.constraints.Pattern;
 import java.time.LocalDate;
 
 @Data
+@NoArgsConstructor
+@AllArgsConstructor
 public class SearchRequest {
 
     @NotBlank(message = "{source.required}")
---
+++ backend/flight_service/src/test/java/com/flightapp/flight_service/TestSuite.java
@@ -0,0 +1,11 @@
+package com.flightapp.flight_service;
+
+import org.junit.platform.suite.api.SelectPackages;
+import org.junit.platform.suite.api.Suite;
+import org.junit.platform.suite.api.SuiteDisplayName;
+
+@Suite
+@SuiteDisplayName("Flight Service Test Suite")
+@SelectPackages("com.flightapp.flight_service")
+public class TestSuite {
+}
