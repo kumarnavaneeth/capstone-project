ba0ebe40bee8d979219a10a73d33c7f8986c9272
anoushkatraining-source <anoushkatraining@gmail.com>
2026-05-05 04:07:51 +0530

    connected api_gateway to ticket_service and flight_service

--- backend/api_gateway/src/app.js
+++ backend/api_gateway/src/app.js
@@ -17,7 +17,7 @@ const userRoutes=require("./routes/userRoutes");
 const flightRoutes=require("./routes/flightRoutes");
 const bookingRoutes=require("./routes/bookingRoutes");
 app.use("/api/v1.0/user",userRoutes);
-app.use("/api/v1.0/flight",flightRoutes);
+app.use("/api/v1.0/flights",flightRoutes); 
 app.use("/api/v1.0/booking",bookingRoutes);
 const PORT=process.env.PORT||8080;
 app.listen(PORT,()=>{
